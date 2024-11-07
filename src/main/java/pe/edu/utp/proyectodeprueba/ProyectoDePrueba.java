6b4275ff01a9955bd037ee5891fadd745c9102b6
MCP75 <cpeche07@gmail.com>
2024-11-06 22:31:07 -0500

    Primera versión

---
+++ src/main/java/pe/edu/utp/proyectodeprueba/ProyectoDePrueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pe.edu.utp.proyectodeprueba;
+
+/**
+ *
+ * @author cpech
+ */
+public class ProyectoDePrueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
